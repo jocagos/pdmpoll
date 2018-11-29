73265be441fd55c8dcd4a1c2a23fc46ec6daf95a
jocagos <zagerer@gmail.com>
2018-11-29 11:52:11 -0600

    Updated files to remove trace of my Azure Connection String

--- JS/connect.js
+++
@@ -1,5 +0,0 @@
-var mongoClient = require("mongodb").MongoClient;
-mongoClient.connect("mongodb://jocagos:Yc8PMCDLfauSIlExUC7QRHXz5iCPSpexIfmTIZOY4ZwWBmV4PoKDE4xllsRGXHABOzkQIZqWlT37LVU9qU3UOg%3D%3D@jocagos.documents.azure.com:10255/?ssl=true", function(err, client) {
-    console.log('Connected?: ' + client.isConnected());
-    client.close();
-});
